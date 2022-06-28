2f4999824c6d33730f36c96073dd60f3d6e327e2
MARY <marymwihaki123@gmail.com>
2022-06-28 17:55:03 -0500

    adding hours array

--- js/app.js
+++ js/app.js
@@ -2,39 +2,72 @@
   FILE app.js
   date 2022-06-27
 */
-
-
-
 'use strict';
 console.log('app is connected');
 
 // The minimum number of customers per hour.
 // The maximum number of customers per hour.
 // The average number of cookies purchased per customer.
+let storeHours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
-function Store(storeLocation , minimumCust, maximumCust, avgCust){
+function Store(storeLocation , minimumCust, maximumCust, avgCookPerCust){
   this.name = storeLocation;
   this.minCust = minimumCust;
   this.maxCust = maximumCust;
-  this.avgCust = avgCust;
-  this.averCooks = [];
-  this.hourlySales = [];
+  this.avgCookPerCust = avgCookPerCust;
+  this.averageCookiesPerHour = [];
   this.TotDailyCooks = 0;
 }
 
-Store.prototype.setCustPerHr = function(){
-  this.CustPerHr = randomCustomer(23,65) + 'cookies.';
+Store.prototype.getHourlySales = function(){
+  return Math.ceil(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  
 };
- 
-function randomCustomer(minCust, maxCust){
-  return Math.floor (Math.random() * (maxCust - minCust +1) + minCust);
 
-}
+// function randomCustomer(minCust, maxCust){
+//   return Math.floor (Math.random() * (maxCust - minCust +1) + minCust);
 
-Store.prototype.openHours = function(){
-  let randomIndexArray= Math.floor((Math.random() * this.hours.length));
-  console.log('openHours:', randomIndexArray);
-};
+// }
+
+// Store.prototype.getAverCooks= function(){
+//   let averCooks= Math.floor((Math.random() * this.hours.length));
+//   console.log('openHours:', averCooks);
+// };
+
+
+
+
+
+
+
+
+
+// ShamanCookieStores.prototype.numOfCustPerHour = function() {
+//   return Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
+// };
+// ShamanCookieStores.prototype.numOfCookiesPerHour = function() {
+//   for(var i=0; i< hours.length; i++){
+//     this.cookiesSold[i]= Math.floor(this.numOfCustPerHour() * this.averageSales);
+//   }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // finding the 'container' in sales.html
@@ -44,27 +77,27 @@ Store.prototype.render = function(){
   let article = document.createElement('article');
   parentElement.appendChild(article);
   // creating a h2, paragraph, ul and li element
-  let eLh2 = document.createElement('h2');
-  eLh2.textContent= this.storeName;
+  let h2El = document.createElement('h2');
+  h2El.textContent= this.storeLocation;
   parentElement.appendChild('eLh2');
   // adding unordered list onto article.
-  let eLp = document.createElement('p');
-  eLp.textContent= 'Welcome to our store in'+ this.storeName;
-  article.appendChild(eLp);
+  let elP = document.createElement('p');
+  elP.textContent= 'Welcome to our store in'+ this.storeLocation;
+  article.appendChild(elP);
   // <ul>
   //   <li></li>
   // </ul>
   // create ul
-  let eLUl = document.createElement('ul');
+  let elUl = document.createElement('ul');
 
-  article.appendChild(eLUl);
+  article.appendChild(elUl);
   // adding listed items onto the unordered list using js
 
   for(let i = 0; i < this.openHours.length; i++){
     let eLLi = document.createElement('li');
 
     eLLi.textContent = this .openHours[i];
-    eLUl.appendChild(eLLi);
+    elUl.appendChild(eLLi);
 
   }
   //insert an image?
@@ -79,7 +112,7 @@ Store.prototype.render = function(){
   let storeTable = document.getElementById('salesTable');
   let storeRow = document.createElement('tr');
   let cellName = document.createElement ('td');
-  cellName.textContent = this.storeName;
+  cellName.textContent = this.storeLocation;
   storeRow.appendChild(cellName);
 
   let maxiCusto = document.createElement('td');
@@ -102,23 +135,18 @@ Store.prototype.render = function(){
 
 let seattle = new Store('Seattle', 23, 65, 6.3);
 
-// let tokyo = new Store();
+let tokyo = new Store('Tokyo',3,24,1.2 );
 
-// let dubai = new Store();
+let dubai = new Store('Dubai',11,38,3.7);
 
-// let paris = new Store();
+let paris = new Store('Paris',20,38,2.3);
 
-// let lima = new Store();
-
-
-// Tokyo	3	24	1.2
-// Dubai	11	38	3.7
-// Paris	20	38	2.3
-// Lima	2	16	4.6
+let lima = new Store('Lima',2,16,4.6);
 
 
 
-let allStores = [seattle];
+
+let allStores = [seattle,tokyo,dubai,paris,lima];
 console.log('Our Stores', allStores);
 
 
