1d53c83780c5a149906724994a526214119f4fcc
MARY <marymwihaki123@gmail.com>
2023-03-23 16:37:18 -0500

    finally works

--- js/app.js
+++ js/app.js
@@ -1,204 +1,94 @@
-/*
-  FILE app.js
-  date 2022-06-27
-*/
 'use strict';
-console.log('app is connected');
-
-
-let parentElement= document.getElementById('store-location-data');
-let hours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-
-
-
-
-
-
-function Store(storeLocation , minCust, maxCust, averageCookies ){
-  this.storeLocation = storeLocation;
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.averageCookies= averageCookies;
+console.log('js connected');
+let parentElement = document.getElementById('store-location-data');
+let hours = [ ' 6am ', ' 7am ', ' 8am ', ' 9am ', '10am', '11am','  12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '  7pm ', ' 8pm ' ,' Total'];
+// console.log(':rocket: ~ file: app.js:5 ~ hours:', hours);
+function Store(storeName,minCustomer,maxCustomer,averageCookies){
+  this.storeName = storeName;
+  this. minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.averageCookies=averageCookies;
   this.totalDailyCookies = 0;
-  this.cookiesPerHour = [];
-  // Store.storeSites.push(this);
+  this.cookiesPerHour=[];
 }
-
-
-function getRandomCustomersPerHour(minCust,maxCust){
-  return Math.ceil(Math.random()* (maxCust- minCust)+minCust);
+//helper function
+function avgcustomerHourly(minCustomer, maxCustomer) {
+  return Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer);
 }
-
-
-
-Store.prototype.cookiesBought = function(){
-  for(let i=0; i < hours.length; i++){
-    this.cookiesPerHour[i]= Math.ceil(getRandomCustomersPerHour(this.minCust, this.maxCust) * this.averageCookies);
+Store.prototype.setCookies = function () {
+  for (let i = 0; i < hours.length; i++) {
+    this.cookiesPerHour[i] = Math.floor(avgcustomerHourly(this.minCustomer, this.maxCustomer) * this.averageCookies);
   }
 };
-
-
-
 let tHead = document.createElement('tr');
 parentElement.appendChild(tHead);
-let tableHeadLocationName = document.createElement('th');
-tableHeadLocationName.textContent='Places';
-tHead.appendChild(tableHeadLocationName);
-//getting hours
-
+let tableNameHead = document.createElement('th');
+tableNameHead.textContent = ('Places');
+tHead.appendChild(tableNameHead);
+//getting the hours
 for(let i =0; i< hours.length; i++){
-  let tableHeadHours = document.createElement('th');
-  tableHeadHours.textContent = hours[i];
+  let tableHeadHours= document.createElement('th');
+  tableHeadHours.textContent= hours[i];
   tHead.appendChild(tableHeadHours);
 }
-
-let seattle = new Store('Seattle', 23, 65, 6.3);
-let tokyo = new Store('Tokyo',3,24,1.2 );
-let dubai = new Store('Dubai',11,38,3.7);
-let paris = new Store('Paris',20,38,2.3);
-let lima = new Store('Lima',2,16,4.6);
-
-
-
-
-Store.prototype.renderStoreInfo = function(){
-  this.cookiesBought();
-
+Store.prototype.render = function(){
+  this.setCookies();
   let storeNameRow = document.createElement('tr');
   parentElement.appendChild(storeNameRow);
-
-
-  let storeHead=document.createElement('th');
-  storeHead.textContent= this.storeLocation;
-  storeNameRow.appendChild(storeHead);
-
-  for(let i = 0; i < hours.length; i++){
-
+  let storeNameHead=document.createElement('th');
+  // adding the names/data of the store to table data
+  storeNameHead.textContent= this.storeName;
+  storeNameRow.appendChild(storeNameHead);
+  for (let i = 0; i < hours.length; i++) {
     this.totalDailyCookies += this.cookiesPerHour[i];
-    console.log(this.cookiesPerHour);
-    let storeDataCell = document.createElement('td');
-    storeDataCell.textContent= this.cookiesPerHour[i];
-    storeNameRow.appendChild(storeDataCell);
+    console.log(this.cookiesPerHour[i]);
+    let storeCell = document.createElement('td');
+    storeCell.textContent = this.cookiesPerHour[i];
+    storeNameRow.appendChild(storeCell);
   }
-  let headingTotal = document.createElement('td');
-  headingTotal.textContent = this.totalDailyCookies;
-  storeNameRow.appendChild('headingTotal');
-};//end main render()
-
-
-// start footer render()
-
-Store.prototype.renderFooter= function(){
+  let totalStoreCookies= document.createElement('td');
+  totalStoreCookies.textContent = this.totalDailyCookies;
+  storeNameRow.appendChild(totalStoreCookies);
+};
+Store.prototype.renderFooter = function(){
   let tFoot = document.getElementById('tableFooter');
+  console.log(':rocket: ~ file: app.js:69 ~ tFoot:', tFoot);
   let footerRow = document.createElement('tr');
-  let totalTitle = document.createElement('th');
-  totalTitle.textContent = 'Total';
-  footerRow.appendChild(totalTitle);
-
-
+  let totalId = document.createElement('th');
+  totalId.textContent = 'Total';
+  footerRow.appendChild(totalId);
   let grandTotal = 0;
   for(let i = 0; i < hours.length; i++){
     let hourlyTotal = 0;
-
+    //go through the cities
+    console.log(allStores);
     for(let j = 0; j < allStores.length; j++){
-      hourlyTotal = hourlyTotal + allStores[j].cookiesPerHour[i];
+      // console.log(allStores[j].cookieTotal[i]);
+      //add to my hourly  total the city total for that hour.
+      //we have two variable i and j, that are arrays.
+      //so go to the citys array and find the number for how many allStores[] which will have Seattl.... and we will look at the total for hour
+      hourlyTotal = hourlyTotal + allStores[j].cookieTotal[i];
+      console.log(':rocket: ~ file: app.js:87 ~ hourlyTotal:', hourlyTotal);
+      //adding up the grand total
     }
     grandTotal += hourlyTotal;
+    //get it out to the page appending it
     let hourlyTotalTd = document.createElement('td');
     hourlyTotalTd.textContent = hourlyTotal;
-    tFoot.appendChild(hourlyTotalTd);
+    footerRow.appendChild(hourlyTotalTd);
   }
-
-let grandTotalRow = documen
-
-
-  let allStores = [seattle,tokyo,dubai,paris,lima];
-  for(let i = 0; i<allStores.length; i++){
-    allStores[i].renderStoreInfo();
-  }
-
-
+  let grandTotalTd = document.createElement('td');
+  grandTotalTd.textContent = grandTotal;
+  footerRow.appendChild(grandTotalTd);
+  tFoot.appendChild(footerRow);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function allowSubmit(evt){
-//   evt.preventDefault();
-//   // retrieving form id's and connecting them to javascript counteparts.
-//   let storeLocation = document.getElementById ('storeLocation').value;
-//   let minimumCust = document.getElementById('minimumCust').value;
-//   let maximumCust = document.getElementById('maximumCust').value;
-//   let avgCookPerCust = document.getElementById ('avgCookPerCust').value;
-
-//   let storePlace  = new Store(storeLocation , minimumCust, maximumCust, avgCookPerCust);
-//   // adding new stores to table
-//   storePlaces.push(storePlace);
-//   Store.renderAll();
-// }
-
-
-
-/**
- *
- */
-// function addLocation(event) {
-//   event.preventDefault();
-//   console.log('In addLocation()');
-//   let good = true; // flags whether the form input is good
-//   let storeLocation = document.getElementById('storeLocation').value.trim();
-//   if (storeLocation === '') {
-//     // Name is required!
-//     good = false;
-//     let span = document.getElementById('storeLocationError');
-//     span.innerText = 'Location cannot be blank!';
-//     let br = document.createElement('br');
-//     span.appendChild(br);
-//   }
-//   let minimumCust = document.getElementById('minimumCust').value;
-//   minimumCust = minimumCust.trim();
-//   let maximumCust = document.getElementById('maximumCustomer');
-//   let avgCookPerCust = document.getElementById('average cookies per customer');
-//   if (good) {
-//     let store = new Store(storeLocation , minimumCust, maximumCust, avgCookPerCust);
-//     form.reset();
-//     storePlaces.push(store);
-//     render(store);
-
-//   }
-// }
-
-/**
- *
- */
-
-
-
-
-
-
-
-
-
-
-
+let storeOne = new Store('Seattle', 23, 65, 6.3);
+let storeTwo = new Store('Tokyo',3,24,1.2 );
+let storeThree = new Store('Dubai',11,38,3.7);
+let storeFour = new Store('Paris',20,38,2.3);
+let storeFive = new Store('Lima',2,16,4.6);
+let allStores = [storeOne, storeTwo, storeThree,storeFour,storeFive];
+console.log(':rocket: ~ file: app.js:43 ~ allStores:', allStores);
+for(let i = 0; i < allStores.length; i++){
+  allStores[i].render();
+}
